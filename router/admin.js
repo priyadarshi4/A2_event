ea5bed5e33e78d644d05b71d1fc210e847dca7cd
Priyadarshi prince <priyadarshiprince5@gmail.com>
2024-12-07 23:11:43 +0530

    Update admin.js

--- router/admin.js
+++ router/admin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { adminModel,validateAdminModel} = require("../models/admin");
 const jwt = require("jsonwebtoken");
 const {validateAdmin , userIsLoggedIn} = require("../middleware/adminValidate");
