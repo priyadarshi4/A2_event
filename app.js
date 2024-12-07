1b314845df1cfbb5c476be71a93066cdc59c6398
Priyadarshi prince <priyadarshiprince5@gmail.com>
2024-12-07 23:12:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const expressSession = require("express-session")
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cookieParser = require("cookie-parser");
 
 require('dotenv').config()
